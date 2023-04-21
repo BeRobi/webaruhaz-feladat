36ea85157760d17df2ebf0f4d0a776ee8569230d
BeRobi <screengsm@gmail.com>
2023-04-21 20:59:35 +0200

    admin oldal hozzáadása

--- adatkezeles.js
+++ adatkezeles.js
@@ -1,3 +1,5 @@
+import { kutyaKULCS } from "./adat.js"
+
 let szamlalo = 0;
 export function kartyaOsszeallit(lista, hova) {
   let kartyakHelye;
@@ -7,36 +9,44 @@ export function kartyaOsszeallit(lista, hova) {
   const KARTYA = elem_letrehozo(kartyakHelye, "div", "kutyaKartya");
   elem_letrehozo(KARTYA, "h3").appendChild(document.createTextNode("Kutya adatai"));
   //lista[i].nev
-  for (const KULCS in lista) {
+  for (const KULCS in kutyaKULCS) {
     elem_letrehozo(KARTYA, "p").appendChild(
-      document.createTextNode(KULCS + ": " + lista[KULCS]));
+      document.createTextNode(kutyaKULCS[KULCS] + ": " + lista[KULCS]));
   }
 }
 
 export function tablazatOsszeallit(lista, hova) {
   let tablaElem = document.querySelector(".tabla");
   if (szamlalo == 0) {
-    tablaElem = elem_letrehozo(hova, "table", "tabla");
-    let tablafejElem = elem_letrehozo(tablaElem, "thead", "tabla");
+    
+    tablaElem = elem_letrehozo(hova, "table");
+    let tablafejElem = elem_letrehozo(tablaElem, "thead", "tablaFej");
     let tablasorElem = elem_letrehozo(tablafejElem, "tr", "fej");
+    tablaElem.classList.add('table', 'table-dark', 'table-striped')
 
-    for (const KULCS in lista)
-      elem_letrehozo(tablasorElem, "th", "aria-sort").appendChild(document.createTextNode(KULCS));
-      elem_letrehozo(tablasorElem, "th").innerHTML = "Művelet";
+    for (const KULCS in kutyaKULCS)
+      elem_letrehozo(tablasorElem, "th", "aria-sort").appendChild(document.createTextNode(kutyaKULCS[KULCS]));
+      elem_letrehozo(tablasorElem, "th", "aria-sort").innerHTML = "Művelet";
   }
   szamlalo += 1;
+  
   let tablatestElem = document.querySelector(".tableBody");
   if (szamlalo == 1)
     tablatestElem = elem_letrehozo(tablaElem, "tbody", "tableBody");
-  const tablasorElem = elem_letrehozo(tablatestElem, "tr", "kutyaTabla");
+  const tablasorElem = elem_letrehozo(tablatestElem, "tr", "sor");
+  //tablaElem.classList.add('table', 'table-dark', 'table-striped')
 
   //lista[i].nev
   for (const KULCS in lista) {
-      elem_letrehozo(tablasorElem, "td", "kutyaCella").appendChild(document.createTextNode(lista[KULCS]));
+      elem_letrehozo(tablasorElem, "td", "cella").appendChild(document.createTextNode(lista[KULCS]));
   }
-  const gombElem = elem_letrehozo(tablasorElem, "td");
-      elem_letrehozo(gombElem, "button", "gomb").innerHTML = "Törlés";
+  const gombHELYE = elem_letrehozo(tablasorElem, "td", "cella");
+      elem_letrehozo(gombHELYE, "button", "gomb").innerHTML = "Törlés"
+      
+     // elem_letrehozo(gombHELYE, "button").classList.add('btn', 'btn-danger')
+     // document.querySelector("button").innerHTML = "Törlés" 
   //console.log(szamlalo);
+  
 }
 
 export function elem_letrehozo(szuloelem, elem, osztaly = "") {
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //import { kutyaLISTA, kutyaKULCS } from "./adat.js";
 import { kartyaOsszeallit, tablazatOsszeallit } from "./adatkezeles.js";
-import { thClick } from "./rendezesSzures.js";
-import { rendezBarmiSzerint } from "./rendezSzur.js";
+//import { thClick } from "./rendezesSzures.js";
+//import { rendezBarmiSzerint } from "./rendezSzur.js";
 // Ami az inportáláshoz kell:
 // az index.html-ben a type="module" attributum használata
 // A importálandó függvény, vagy változó neve elé az export kuclsszó
@@ -11,15 +11,15 @@ window.addEventListener("load", jsonAdatok);
 
 let kartyak = document.querySelector(".kartyak");
 let tablazatSection = document.querySelector(".tablazat");
-let tabla = document.querySelector(".tabla");
+let tabla = document.querySelector("table");
 
 let kutyaLISTA = [];
 
 function jsonAdatok() {
   let vegpont = "adat.json";
   adatBeolvas(vegpont, init);
- 
 }
+
 
 function adatBeolvas(vegpont, callbackFv) {
   fetch(vegpont, {
@@ -32,125 +32,12 @@ function adatBeolvas(vegpont, callbackFv) {
     .catch((err) => console.log(err));
 }
 
+
 function init(data) {
   kutyaLISTA = data;
   console.log(kutyaLISTA)
-
-  //const SZURES = document.querySelector(".kor")
-  //SZURES.addEventListener("click", rendezBarmiSzerint);
-
   for (const kutya of kutyaLISTA) {
     kartyaOsszeallit(kutya, kartyak);
-    tablazatOsszeallit(kutya, tablazatSection);
+    //tablazatOsszeallit(kutya, tablazatSection); 
   }
-
-  for (const th of document.querySelectorAll(".aria-sort")) {
-    th.addEventListener("click", thClick);
-  }
-
-  torlesFunkcio(kutyaLISTA);
-
-  const SUBMIT = document.querySelector("#rogzites");
-  SUBMIT.addEventListener("click", ujKutya);
-}
-
-function ujKutya() {
-  let kutya = {};
-
-  let szuka = document.querySelector("#szuka");
-  let kan = document.querySelector("#kan");
-  let kuldheto;
-  let hibauzenet = "";
-  /**szedjük össze az űrlap adatait,
-   * és tegyük bele egy objektumba
-   * és fűzzük hozá a KUTYALISTA-hoz
-   */
-  const NevInputElem = document.querySelector("#kneve");
-  const FajInputElem = document.querySelector("#kfajta");
-  const LabInputElem = document.querySelector("#klaba");
-  const MagInputElem = document.querySelector("#mmag");
-  const KorInputElem = document.querySelector("#kkor");
-
-  //itt is érdemes ellenőrizni, hogy megfelelő-e az adat:
-  var filter = /[a-zA-Z]/; //^[A-Z]{2,}$
-  if (filter.test(FajInputElem.value)) {
-    kutya.nev = NevInputElem.value;
-    kutya.fajta = FajInputElem.value;
-    kutya.lab = LabInputElem.value;
-    kutya.marmagassag = MagInputElem.value;
-
-    document.querySelector("#nevhiba").innerHTML = "";
-    kuldheto = true;
-  } else {
-    kuldheto = false;
-    hibauzenet = "A név hiányzik, vagy a formátuma hibás!";
-    document.querySelector("#nevhiba").innerHTML = hibauzenet;
-  }
-
-  const NemInputElem = document.querySelector("#szuka");
-  if (NemInputElem.checked) {
-    kutya.nem = "szuka";
-  } else {
-    kutya.nem = "kan";
-  }
-  kutya.kor = KorInputElem.value;
-  console.log(kutya);
-  if (kuldheto) {
-    kutyaLISTA.push(kutya);
-  }
-
-  kartyaOsszeallit(kutya, kartyak);
-  tablazatOsszeallit(kutya, tabla);
-  torlesFunkcio(kutyaLISTA);
-
-  console.log(kutyaLISTA);
-}
-
-function torlesFunkcio(lista) {
-  let TORLES = document.querySelectorAll(".gomb");
-  console.log(TORLES);
-  for (let i = 0; i < lista.length; i++) {
-    let kutya = lista[i];
-    // let TABLA = document.querySelectorAll(".kutyaTabla")[i];
-    // let KARTYA = document.querySelectorAll(".kutyaKartya")[i];
-
-    TORLES[i].addEventListener("click", torlesClick);
-  }
-}
-
-function torlesClick(event) {
-  let target = event.target;
-  let td = target.parentElement;
-  let tr = td.parentElement;
-  console.log("tr", tr.parentElement);
-
-  var child = td;
-  var idx = tr.rowIndex - 1;
-
-  console.log("idx", idx, kutyaLISTA[idx]);
-  let kutya = kutyaLISTA[idx];
-  console.log(kutya);
-  // let  = target.parentElement.parentElement
-
-  tr.remove();
-
-  let kartya = document.querySelectorAll(".kutyaKartya")[idx];
-  kartya.remove();
-
-  // console.log("target", target)
-
-  // console.log("target parent", tr)
-  //if (document.contains(TORLES)) {TABLA.remove(), KARTYA.remove()}
-  // console.log(">>", TABLA.parentElement, KARTYA.parentElement);
-  // TABLA.parentElement.removeChild(TABLA);
-  // KARTYA.parentElement.removeChild(KARTYA);
-  // let index = kutyaLISTA.indexOf(kutya);
-
-  console.log(idx);
-  kutyaLISTA.splice(idx, 1);
-
-  console.log("kutya", kutya);
-  //console.log(index);
-  //console.log(kutyaLISTA[i]);
-  console.log("kutyalista", kutyaLISTA);
 }
