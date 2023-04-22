69dcafe13f2f8ec9a773527df3774a38404fe511
BeRobi <screengsm@gmail.com>
2023-04-22 07:11:24 +0200

    űrlap formázása

--- adat.js
+++ adat.js
@@ -52,9 +52,9 @@ export const kutyaLISTA = [
   export const kutyaKULCS={
    
     nev: "Név",
-    kor: "Kor",
     fajta: "Fajta",
     lab: "Láb",
+    marmagassag: "Marmagasság",
     nem: "Ivar",
-    marmagassag: "Marmagasság"
+    kor: "Kor"
 }
